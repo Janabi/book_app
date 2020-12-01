4e1b744f623ae421932445d1d33da811e3431f28
Abd Alrhman <abdalrhman@Abds-MacBook-Pro.local>
2020-12-01 17:07:09 +0200

    lab 13 is done

--- public/app.js
+++ public/app.js
@@ -1,44 +1,44 @@
 $(function(){
+    // toggle method to open and close the hamburger menu
     $('.hamburger div').on('click', function() {
         $('.list').slideToggle(500);
     });
 
 });
 
-// Get the modal
+// Get the update form
 var form = document.getElementById("updateFrom");
 
-// Get the button that opens the modal
+// Get the button that opens the update form
 var btn = document.getElementById("showUpdateForm");
 
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the form Update 
 btn.onclick = function() {
   form.style.display = "block";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the delete and update form, close it
 window.onclick = function(event) {
-  if (event.target == form) {
+  if (event.target == form || event.target == formDel) {
     form.style.display = "none";
+    formDel.style.display = "none";
   }
 }
 
-// Get the modal
+// Get the delete form
 var formDel = document.getElementById("popupMessage");
 
-// Get the button that opens the modal
+// Get the button that opens the delete form
 var btnDel = document.getElementById("btnDel");
 
 
-// When the user clicks the button, open the modal 
+// When the user clicks the button, open the form Delete 
 btnDel.onclick = function() {
   formDel.style.display = "block";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == formDel) {
+// When the user clicks the button, close the form Delete
+btnNo.onclick = function() {
     formDel.style.display = "none";
   }
-}
